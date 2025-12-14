e4e00590e92fa005b2f588b1ef1d082fc0e99d75
eleocraft <eliseo.cailloux@gmx.de>
2025-12-14 19:30:19 +0100

    adding deps for can_config

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ mod definitions;
 mod beacons;
 
 pub mod command_types;
+#[cfg(feature = "embedded")]
 pub mod can_config;
 
 // public reexports
+pub use definitions::{telemetry, telecommands};
 pub use tmtc_system::TMValue;
 pub use tmtc_system::DynTMValue;
 pub use tmtc_system::DynBeacon;
 pub use tmtc_system::DynTelemetryDefinition;
 pub use beacons::low_rate_telemetry::LowRateTelemetry;
 pub use beacons::mid_rate_telemetry::MidRateTelemetry;
-
